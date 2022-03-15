cd98cc627ee479e000d5f56da1b4820bd9665846
Marcin <alpinus1@gmail.com>
2022-03-15 11:49:21 +0100

    css border fix; win conditions

--- script.js
+++ script.js
@@ -25,25 +25,77 @@ const currentPlayerTurn = () => {
 // statusdisplay pod naszym .game--status bedzie wyswietlał zmienną currentPlayerTurn
 statusDisplay.innerHTML = currentPlayerTurn();
 
-//
-function handleCellPlayed() {}
+//handler kliknietej cellki przyjmujacy dwie wartosci -> clickedCell i clickedCellIndex
+function handleCellPlayed(clickedCell, clickedCellIndex) {
+	gameState[clickedCellIndex] = currentPlayer;
+	clickedCell.innerHTML = currentPlayer;
+}
 
 // fn - zmiana gracza
 function handlePlayerChange() {}
 
 // fn - sprawdzanie rezultatu
-function handleResultValidation() {}
+
+// wartosci w tablicy winningConditions to indeksy dla komorek ktore musza byc zaznaczone przez TEGO SAMEGO gracza żeby był uznany jako zwycięzca
+const winningConditions = [
+	[0, 1, 2],
+	[3, 4, 5],
+	[6, 7, 8],
+	[0, 3, 6],
+	[1, 4, 7],
+	[2, 5, 8],
+	[0, 4, 8],
+	[2, 4, 6],
+];
+// sprawdzamy wynik
+function handleResultValidation() {
+	let roundWon = false;
+	// i <= 7 ponieważ mamy indeksy naszych tablic od 0-7 więc iterujemy po kazdej naszej tablicy sprawdzając poprawnosc wyniku
+	for (let i = 0; i <= 7; i++) {
+		const winCondition = winningConditions[i];
+		let a = gameState[winCondition[0]];
+		let b = gameState[winCondition[1]];
+		let c = gameState[winCondition[2]];
+
+		if (a === '' || b === '' || c === '') {
+			continue;
+		}
+		if (a === b && b === c) {
+			roundWon = true;
+			break;
+		}
+	}
+	// jeżeli są spełnione warunki zwycięstwa dla jednego z graczy gameActive przyjmie wartosc false, innerHTML wyświetli winningMessage, funkcja zostanie zwrocona
+	if (roundWon) {
+		statusDisplay.innerHTML = winningMessage();
+		gameActive = false;
+		return;
+	}
+}
 
 // fn - klikniecie komorki => listener na wszystkie nasze komórki wywołujący tą funkcję \/
-function handleCellClick() {}
+function handleCellClick(clickedCellEvent) {
+	// kliknieta cellka
+	const clickedCell = clickedCellEvent.target;
+	// index celki => musimy zamienić stringa z data-cell-index na number => parseInt (bez tego bysmy otrzymywali numery ale w stringu)
+
+	const clickedCellIndex = parseInt(
+		clickedCell.getAttribute(`data-cell-index`)
+	);
+
+	// sprawdzamy czy ktoras komorka została już kliknięta lub czy gra nie jest zapauzowana => jeżeli tak to ignorujemy kliknięcie.
+	// => jeżeli - dana kliknięta komórka NIE JEST pusta lub gra nie jest aktywna to zwraca nam spowrotem funkcję
+
+	if (gameState[clickedCellIndex] !== '' || !gameActive) {
+		return;
+	}
+	// jezeli nasz if nie jest spełniony możemy kontynuować grę i wywołujemy funkcję handleResultValidation
+	handleCellPlayed(clickedCell, clickedCellIndex);
+	handleResultValidation();
+}
 
 // fn - restart gry
 function handleRestartGame() {}
-
-
-
-
-
 
 //
 //
